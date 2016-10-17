af9a7c41e31afbe88644719008955632098b353f
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-10-17 22:52:27 +0200

    Extend reducer. Add Button component

--- pomodoro-test.js
+++ pomodoro-test.js
@@ -11,5 +11,44 @@ const testDisplayClick = () => {
   stateAfter = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 10, relaxTime: 5}};
   expect(pomodoro(stateBefore, action)).toEqual(stateAfter);
 }
+
+const testIncreaseTime = () => {
+  let stateBefore = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 10, relaxTime: 5}};
+  let stateAfter = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 11, relaxTime: 5}};
+  let action = {type: 'INCREASE_WORK'}
+  expect(pomodoro(stateBefore, action)).toEqual(stateAfter);
+}
+
+const testDecreaseTime = () => {
+  let stateBefore = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 10, relaxTime: 5}};
+  let stateAfter = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 9, relaxTime: 5}};
+  let action = {type: 'DECREASE_WORK'}
+  expect(pomodoro(stateBefore, action)).toEqual(stateAfter);
+  stateBefore = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 0, relaxTime: 5}};
+  stateAfter = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 0, relaxTime: 5}};
+  expect(pomodoro(stateBefore, action)).toEqual(stateAfter);
+}
+
+const testIncreaseRelax = () => {
+  let stateBefore = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 10, relaxTime: 5}};
+  let stateAfter = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 10, relaxTime: 6}};
+  let action = {type: 'INCREASE_RELAX'}
+  expect(pomodoro(stateBefore, action)).toEqual(stateAfter);
+}
+
+const testDecreaseRelax = () => {
+  let stateBefore = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 10, relaxTime: 5}};
+  let stateAfter = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 10, relaxTime: 4}};
+  let action = {type: 'DECREASE_RELAX'}
+  expect(pomodoro(stateBefore, action)).toEqual(stateAfter);
+  stateBefore = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 10, relaxTime: 0}};
+  stateAfter = {activity: 'work', timeLeft: 10, status: 'off', defaultTime: { workTime: 10, relaxTime: 0}};
+  expect(pomodoro(stateBefore, action)).toEqual(stateAfter);
+}
+
 testDisplayClick();
+testIncreaseTime();
+testDecreaseTime();
+testIncreaseRelax();
+testDecreaseRelax();
 console.log('tests pass');
--- pomodoro.js
+++ pomodoro.js
@@ -1,7 +1,7 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
 
-const {createStore} = require('redux');
+const { createStore } = require('redux');
 
 //let defaultTime = {workTime: 10, relaxTime: 5};
 
@@ -12,12 +12,30 @@ const pomodoro = (state = {activity: 'work', timeLeft: 10, status: 'off', defaul
         return Object.assign({}, state, {status: 'on'});
       }
       return Object.assign({}, state, {status: 'off'});
+    case 'INCREASE_WORK':
+      return Object.assign({}, state, {defaultTime: {workTime: state.defaultTime.workTime +1, relaxTime: state.defaultTime.relaxTime}})
+    case 'DECREASE_WORK':
+      if (state.defaultTime.workTime > 0) {
+        return Object.assign({}, state, {defaultTime: {workTime: state.defaultTime.workTime - 1, relaxTime: state.defaultTime.relaxTime}})
+      }
+      return state;
+    case 'INCREASE_RELAX':
+      return Object.assign({}, state, {defaultTime: {workTime: state.defaultTime.workTime, relaxTime: state.defaultTime.relaxTime + 1}})
+    case 'DECREASE_RELAX':
+      if (state.defaultTime.relaxTime > 0) {
+        return Object.assign({}, state, {defaultTime: {workTime: state.defaultTime.workTime, relaxTime: state.defaultTime.relaxTime - 1}})
+      }
+      return state;
+      
+    default:
+      return state;
   }
 }
 
 module.exports = {
     pomodoro
 };
+
 const store = createStore(pomodoro);
 
 // store.subscribe(() => console.log(store.getState()));
@@ -26,35 +44,41 @@ const store = createStore(pomodoro);
 // store.dispatch({type: 'PAUSE'});
 // store.dispatch({type: 'PAUSE'});
 
+const Button = ({
+  type,
+  value
+}) => (
+  <button 
+    onClick={()=> {
+      store.dispatch({
+        type:type,
+      })
+    }}>
+    {value}
+  </button>
+  )
 
-const TimerSettings = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <button> + </button>
-        <span>Work Time {this.props.defaultTime.workTime}</span>
-        <button> - </button>
-        <p></p>
-        <button> + </button>
-        <span>Relax Time {this.props.defaultTime.relaxTime}</span>
-        <button> - </button>
-      </div>
-    );
-  }
-});
+const TimerSettings = ({defaultTime}) => (
+  <div>
+    <Button type='INCREASE_WORK' value='+' />
+    <span>Work Time {defaultTime.workTime}</span>
+    <Button type='DECREASE_WORK' value='-' />
+    <p></p>
+    <Button type='INCREASE_RELAX' value='+' />
+    <span>Relax Time {defaultTime.relaxTime}</span>
+    <Button type='DECREASE_RELAX' value='-' />
+  </div>
+  );
 
-const TimerDisplay = React.createClass({
-  render: function() {
-    return (
-      <div
-      onClick={()=>{
-        store.dispatch({type:'PAUSE'})
-      }}>
-        { this.props.timeLeft }
-      </div>
-    );
-  }
-});
+const TimerDisplay = ({timeLeft}) => (
+  <div
+    onClick={()=>{
+      store.dispatch({type:'PAUSE'})
+    }}>
+    { timeLeft }
+  </div>
+);
+
 
 const Pomodoro = React.createClass({
   getInitialState: function() {
